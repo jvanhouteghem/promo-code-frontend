f71bee92a1da43acab4b415de0329c09b1a334a2
VanhouteghemJ <vanhouteghem.j@gmail.com>
2023-05-05 22:55:37 +0200

    feat(init-project): integ test of external react-form-catalog library

--- src/pages/promo-code/create/index.tsx
+++ src/pages/promo-code/create/index.tsx
@@ -1,7 +1,10 @@
+import PromoCodeForm from "@/pages/promo-code/create/components/form";
+
 export default function CreatePromoCode(): JSX.Element {
     return (
         <>
             CreatePromoCode
+            <PromoCodeForm />
         </>
     )
 }
