10164621bf3efad07fed10be99d06ca557a3ff8f
VanhouteghemJ <vanhouteghem.j@gmail.com>
2023-05-08 21:20:54 +0200

    feat(clean-check-code-hook): create promo-code-input hook

--- src/pages/promo-code/components/card-order-summary/card-order-summary.tsx
+++ src/pages/promo-code/components/card-order-summary/card-order-summary.tsx
@@ -1,63 +1,15 @@
 import './card-order-summary.scss'
 import {OrderSummaryItem} from "@/pages/promo-code";
-import {useEffect, useState} from "react";
-import {
-    Box,
-    Button,
-    CircularProgress,
-    TextField
-} from "@mui/material";
-import {checkPromoCode} from "@/pages/api/promo-code/services/promo-code.service";
+import {Button} from "@mui/material";
+import {PromoCode} from "@/pages/promo-code/components/card-order-summary/components/promo-code-input";
+import {usePromoCodeInput} from "@/pages/promo-code/components/card-order-summary/components/promo-code-input.hook";
 
 export default function CardOrderSummaryPage({orderSummaryItems, resetOrders}: {orderSummaryItems: OrderSummaryItem[]; resetOrders: any}): JSX.Element {
-    const [promoCode, setPromoCode] = useState('');
-    const [promoCodeFetched, setPromoCodeFetched] = useState<{ isValid?: boolean; result?: number; value?: any }>({});
-    const [promoCodeValidatorAttributes, setPromoCodeValidatorAttributes] = useState<any>({
-        label:"Add promo code here.",
-        error: null,
-        helperText: null,
-        color: null,
-        focused: null
-    });
-
-
-    const [isCheckingPromoCode, setIsCheckingPromoCode] = useState(false);
-    useEffect(() => {
-        if (promoCode) {
-            const handler = setTimeout(() => {
-                setIsCheckingPromoCode(true);
-                const sumWithoutPromo = orderSummaryItems?.reduce((accumulator, currentValue) => accumulator + (currentValue.quantity * currentValue.price), 0) ?? 0;
-                checkPromoCode(promoCode, sumWithoutPromo).then((data: any) => {
-                    setIsCheckingPromoCode(false);
-                    setPromoCodeFetched(data);
-                    console.log('promoCodeFetched', data)
-                    if (!promoCode) {
-                        setPromoCodeFetched({});
-                        setPromoCodeValidatorAttributes({...promoCodeValidatorAttributes, error: null, helperText: null, color: null, focused: null});
-                    } else if (data.isValid) {
-                        setPromoCodeValidatorAttributes({...promoCodeValidatorAttributes, error: false, helperText: `A ${data.value.amount * 100}% discount has been applied!`, color: "success", focused: true});
-                    } else {
-                        setPromoCodeValidatorAttributes({...promoCodeValidatorAttributes, error: true, helperText:"Please add a valid promo code.", color: null, focused: null});
-                    }
-                });
-            }, 850);
-
-            return () => {
-                clearTimeout(handler);
-            };
-        } else {
-            setPromoCodeFetched({});
-            setPromoCodeValidatorAttributes({...promoCodeValidatorAttributes, error: null, helperText: null, color: null, focused: null});
-        }
-    }, [promoCode, orderSummaryItems]);
+    const {promoCodeFetched, isCheckingPromoCode ,} = usePromoCodeInput(orderSummaryItems);
 
     function sum(orderSummaryItems: OrderSummaryItem[]): number {
         const sumWithoutPromoo = orderSummaryItems?.reduce((accumulator, currentValue) => accumulator + (currentValue.quantity * currentValue.price), 0) ?? 0;
-        return promoCodeFetched.result ? promoCodeFetched.result : sumWithoutPromoo; // ? sumWithoutPromo * (1 - isPromo) : sumWithoutPromo;
-    }
-
-    function handleChange(event: any) { // ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-        setPromoCode(event.target.value);
+        return promoCodeFetched.result ? promoCodeFetched.result : sumWithoutPromoo;
     }
 
     function handleSubmit(event: any) { // React.FormEvent
@@ -77,31 +29,14 @@ export default function CardOrderSummaryPage({orderSummaryItems, resetOrders}: {
                                         <div className="quantity">x{orderSummaryItem.quantity}</div>
                                         <div className="label">{orderSummaryItem.label}</div>
                                     </div>
-                                    <div data-test-id={`cardOrderSummaryShipItemPrice-${index}`}className="price">${orderSummaryItem.price.toFixed(2)}</div>
+                                    <div data-test-id={`cardOrderSummaryShipItemPrice-${index}`} className="price">${orderSummaryItem.price.toFixed(2)}</div>
                                 </div>
                             ))}
                     </div>
                 </div>
 
                 <div className="promo-code-input-container">
-                        <Box sx={{ position: 'relative' }}>
-                        <TextField
-                            id="outlined-error-helper-text"
-                            disabled={isCheckingPromoCode}
-                            value={promoCode}
-                            onChange={event => handleChange(event)}
-                            style={{width: '100%'}}
-                            {...promoCodeValidatorAttributes}
-                        />
-                        {isCheckingPromoCode && <CircularProgress
-                            size={30}
-                            sx={{
-                                position: 'absolute',
-                                marginTop: '13px',
-                                marginLeft: '-125px',
-                            }}
-                        />}
-                    </Box>
+                    <PromoCode orderSummaryItems={orderSummaryItems} />
                </div>
 
                 <div className="sum-total">
